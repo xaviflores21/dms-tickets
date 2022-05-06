b746d05c3b48d1ef795ade51e77f941e221d6495
Javier W. Flores <willams_21@hotmail.com>
2022-05-06 04:11:36 -0400

    paquetes

---
+++ dms-tickets/Reservas.Domain/ValueObjects/NumeroTicket.cs
@@ -0,0 +1,33 @@
+﻿using ShareKernel.Core;
+using ShareKernel.Rules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservas.Domain.ValueObjects
+{
+    public record NumeroTicket : ValueObject
+    {
+        public string Value { get; }
+
+        public NumeroTicket(string value)
+        {
+            CheckRule(new StringNotNullOrEmptyRule(value));
+            Value = value;
+        }
+
+        public static implicit operator string(NumeroTicket value)
+        {
+            return value.Value;
+        }
+
+        public static implicit operator NumeroTicket(string value)
+        {
+            return new NumeroTicket(value);
+        }
+
+
+    }
+}
